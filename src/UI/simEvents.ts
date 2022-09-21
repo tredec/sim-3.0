035b33c1b7747a31646bf6697874938adcc57c65
tredec <robin.glowacz1@gmail.com>
2022-09-21 12:15:04 +0200

    Output Text will now only get removed after sim if no error was thrown

--- src/UI/simEvents.ts
+++ src/UI/simEvents.ts
@@ -28,7 +28,7 @@ const ddtOtp = qs(".ddtOtp");
 const tableHeaders = {
   current: "All",
   single: "<th>Theory</th><th>&sigma;<sub>t</sub></th><th>Last pub</th><th>Max Rho</th><th>&Delta;&tau;</th><th>Multi</th><th>Strat</th><th>&tau;/h</th><th>Pub Time</th>",
-  all: "<th>&emsp;</th><th>Input</th><th>&tau;/h Active</th><th>&tau;/h Idle</th><th>Ratio</th><th>Multi Active</th><th>Multi Idle</th><th>Strat Active</th><th>Strat Idle</th><th>Time Active</th><th>Time Idle</th><th>&Delta;&tau; Active</th><th>&Delta;&tau; Idle</th>"
+  all: "<th>&emsp;</th><th>Input</th><th>&tau;/h Active</th><th>&tau;/h Idle</th><th>Ratio</th><th>Multi Active</th><th>Multi Idle</th><th>Strat Active</th><th>Strat Idle</th><th>Time Active</th><th>Time Idle</th><th>&Delta;&tau; Active</th><th>&Delta;&tau; Idle</th>",
 };
 thead.innerHTML = tableHeaders.all;
 table.classList.add("big");
@@ -43,39 +43,39 @@ event(simulateButton, "click", async () => {
     mode: mode.value,
     modeInput: modeInput.value,
     hardCap: hardCap.checked,
-    global: { dt: parseFloat(dtOtp.textContent ?? "1.5"), ddt: parseFloat(ddtOtp.textContent ?? "1.0001"), stratFilter: true }
+    global: { dt: parseFloat(dtOtp.textContent ?? "1.5"), ddt: parseFloat(ddtOtp.textContent ?? "1.0001"), stratFilter: true },
   };
   output.textContent = "";
   simulateButton.textContent = "Stop simulating";
   await sleep();
   let res = await simulate(data);
   if (typeof res === "string") output.textContent = res;
-  else if (res !== null) updateTable(res);
+  else output.textContent = "";
+  if (res !== null && typeof res !== "string") updateTable(res);
   simulateButton.textContent = "Simulate";
-  output.textContent = "";
 });
 
-function updateTable(arr: Array<simResult>):void {
-  if(arr[0].length !== thead.children[0].children.length){
-    if(arr[0].length === 10) {
-      thead.innerHTML = tableHeaders.single
-      table.classList.remove("big")
+function updateTable(arr: Array<simResult>): void {
+  if (arr[0].length !== thead.children[0].children.length) {
+    if (arr[0].length === 10) {
+      thead.innerHTML = tableHeaders.single;
+      table.classList.remove("big");
       table.classList.add("small");
-      clearTable()
+      clearTable();
     }
   }
-  for(let i = 0; i < arr.length;i++){
-    const row = <HTMLTableRowElement>ce("tr")
-    for(let j = 0; j < thead.children[0].children.length;j++){
-       const cell = ce("td")
-       cell.innerText = String(arr[i][j])
-       row.appendChild(cell)
-  }
-  tbody.appendChild(row)
+  for (let i = 0; i < arr.length; i++) {
+    const row = <HTMLTableRowElement>ce("tr");
+    for (let j = 0; j < thead.children[0].children.length; j++) {
+      const cell = ce("td");
+      cell.innerText = String(arr[i][j]);
+      row.appendChild(cell);
+    }
+    tbody.appendChild(row);
   }
   console.log(arr);
 }
 
-function clearTable():void{
-  while(tbody.firstChild)tbody.firstChild.remove()
+function clearTable(): void {
+  while (tbody.firstChild) tbody.firstChild.remove();
 }
